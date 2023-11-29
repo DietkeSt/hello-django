aade01387ded19f2adec05ed946cb79d808ab72a
DietkeSt <>
2023-11-29 17:50:27 +0000

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-s*-m7-14r&_tm$1d&7p=+fs+&13gwe$d!7hboq(gia)%mpw7k0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dietkest-hello-django-emyshyylf3.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-dietkest-hello-django-emyshyylf3.us2.codeanyapp.com',
+                 'https://dietke-django-todo-app-1df14e3bd2d7.herokuapp.com/']
 
 
 # Application definition
