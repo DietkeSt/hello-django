2d128d55532707aab96158f78f7c1ac249aeec9e
DietkeSt <>
2023-11-29 18:06:01 +0000

    Setup automatic deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s*-m7-14r&_tm$1d&7p=+fs+&13gwe$d!7hboq(gia)%mpw7k0'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-s*-m7-14r&_tm$1d&7p=+fs+&13gwe$d!7hboq(gia)%mpw7k0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dietke-django-todo-app-1df14e3bd2d7.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
